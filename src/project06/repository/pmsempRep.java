c35e1575ae7e3c6a3b33db086d466634de3710b0
skxowns19 <60504099+skxowns19@users.noreply.github.com>
2020-05-14 17:28:21 +0900

    0514

--- src/project06/repository/pmsempRep.java
+++ src/project06/repository/pmsempRep.java
@@ -9,13 +9,17 @@ import project06.vo.pmsempSch;
 
 @Repository
 public interface pmsempRep {
+	// CEO, CTO, PM 리스트
 	public ArrayList<pmsemp> pmsempList(pmsemp sch);
+	// PM 삭제 리스트
 	public ArrayList<pmsemp> pmempList(pmsemp sch);
-	
+	// CEO, CTO 사원추가
 	public void insert(pmsemp ins);
-	
+	// CEO CTO설정
 	public void updatepmsemp1(pmsemp upt);
 	public void updatepmsemp2();
+	// PM 팀원 삭제
+	public void delemp(pmsemp del);
 	
 	// Jin
 	public int pmsEmpCounter(pmsemp emp);
