4abd7dfbf34ef629a97e9ae9c9c669eaf7bbfbce
dlwns1541 <64745238+dlwns1541@users.noreply.github.com>
2020-05-18 13:54:05 +0900

    s

---
+++ src/project06/repository/RiskDao.java
@@ -0,0 +1,36 @@
+package project06.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Repository;
+
+import project06.vo.Comment;
+import project06.vo.Risk;
+import project06.vo.RiskSch;
+
+@Repository
+public interface RiskDao {
+	
+	public int totCnt(RiskSch sch);
+	public int ctotCnt(RiskSch sch);
+
+	public ArrayList<Risk> list(RiskSch sch);
+	
+	public void insert(Risk ins);
+	public void cinsert(Comment ins);
+	
+	public Risk getRisk(int ino);
+	
+	public void updateRisk(Risk upt);
+	public void comupdate(Risk upt2);
+	// 업로드
+	public void insertRepo(String fname);
+	public ArrayList<String> fnames(int ino);
+	public void uptFileInfo(HashMap hm);
+	public void insFileInfo(HashMap hm);
+	// 댓글
+	public ArrayList<Comment> clist(RiskSch sch);
+	// 조회수
+	public void uptReadCnt(int ino);
+}
