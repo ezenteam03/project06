9c2350cfc41b66a038f33eb4f93d8174b3032443
rujane80 <50623207+rujane80@users.noreply.github.com>
2020-05-21 14:02:13 +0900

    11

--- src/project06/service/DashTeamService.java
+++ src/project06/service/DashTeamService.java
@@ -25,4 +25,41 @@ public class DashTeamService {
 	public ArrayList<DashRe> rlist(int pno) {
 		return dao.rlist(pno);
 	}
+	public ArrayList<DashTeam> tlist(int eno) {
+		ArrayList<DashTeam> tasks = dao.tlist(eno);
+		for(int i=0; i<tasks.size(); i++) {
+			if(tasks.get(i).getGrade()<0 && (tasks.get(i).getProg() != 100 || !tasks.get(i).getTdiv().equals("결재완료")) ) {
+				tasks.get(i).setGrade(2);
+				continue;
+			}
+		}
+		return tasks;
+	}
+	public DashProject getProject(int pno) {
+		ArrayList<DashTeam> plist = dao.plist(dao.pm(pno));
+		DashProject p = dao.getProject(pno);
+		p.setGrade(0);
+		int tleng=0;
+		int prog=0;
+		int dleng=0;
+		for(int i=0; i<plist.size(); i++) {
+			if(plist.get(i).getGrade()<0 && (plist.get(i).getProg() != 100 || !plist.get(i).getTdiv().equals("결재완료"))) {
+				p.setGrade(2);
+			} 
+			tleng += plist.get(i).getTleng();
+			if(plist.get(i).getTdiv().equals("결재완료")) {
+				dleng += plist.get(i).getTleng();
+				prog += 100*plist.get(i).getTleng();
+			}
+			else if(plist.get(i).getDleng()>0) {
+				dleng += plist.get(i).getDleng();
+				prog += plist.get(i).getProg()*plist.get(i).getTleng();
+			}
+		}
+		prog = (int) prog/tleng;
+		int dprog = (int)(dleng/tleng);
+		if(prog+10<dprog&&p.getGrade()!=2) p.setGrade(1);
+		p.setProg(prog);
+		return p;
+	}
 }
