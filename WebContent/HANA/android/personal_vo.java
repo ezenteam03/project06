30e6abc8895d5ea36056b9ed4ffd54a5b30ca0c3
Hana <64818459+Hana-Kimma@users.noreply.github.com>
2020-05-21 14:20:09 +0900

    1

---
+++ WebContent/HANA/android/personal_vo.java
@@ -0,0 +1,20 @@
+package com.example.hfinal;
+
+public class personal_vo {
+    private String topic;
+
+    public personal_vo(String topic) {
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+}
+
+
+
