bf85678f3f6e2b44f4715f60c130a8d4ae5ab682
Hana <64818459+Hana-Kimma@users.noreply.github.com>
2020-06-03 11:55:48 +0900

    123

--- src/project06/repository/MeetDao.java
+++ src/project06/repository/MeetDao.java
@@ -20,16 +20,27 @@ public interface MeetDao {
 	public void insert(Meet ins);
 	public void updateMeet(Meet upt);
 	public void deleteMeet(int mnno);	
-	// 파일 정보 등록..
+	public Meet getMeet(int mnno);
+	
+	// 업로드
+		public void insertRepo(String fname);		// 파일업로드
+		public String fnames(int mnno);	// 내용출력
+		public void uptFileInfo(HashMap hm);		// 업로드된 파일 수정
+		public void insFileInfo(HashMap hm);		// 수정된 내용 업로드
+		
+	
+	
+	
+	/* 파일 등록 수정 전
 	public void insertRepo(String fname);
 	
-	public Meet getMeet(int mnno);
 	// 파일 다운로드 정보 
 	public String fnames(int mnno);
 	public void uptFileInfo(HashMap hm);
 	public void insFileInfo(HashMap hm);
-	public void uptReadCnt(int mnno);
+	*/
 	
+	public void uptReadCnt(int mnno);
 	public ArrayList<Meet> andlist(Meet sch);
 	
 }
